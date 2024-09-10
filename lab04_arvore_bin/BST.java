ee94c04de95b29b6b63a58e8437fbd4095184cd5
Alan Gleizer <alan@mgleizer.com>
2024-09-10 16:38:07 -0300

    busca em BST funcionando

--- lab04_arvore_bin/BST.java
+++ lab04_arvore_bin/BST.java
@@ -9,6 +9,26 @@ public class BST extends BinaryTree{
         this.setRoot(node);
     }
 
+    public Node search(String data) {
+        Node current = this.getRoot();
+        // loop para percorrer a arvore
+        while (current != null) {
+            int result = current.getData().compareTo(data); // comparar dado do Node com a pesquisa
+                                                            // retorna 0 se strings iguais
+                                                            // +1 se Node > pesquisa, -1 se Node < pesquisa 
+
+            if (result == 0) {
+                return current;
+            } else if (result > 0) {
+                current = current.getLeft();
+            } else {
+                current = current.getRight();
+            }
+        }
+
+        return null; // se sairmos do loops, não encontramos o dado
+    }
+
     // 
 
 
